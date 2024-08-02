707c56f20e21dc2c9fc6b410fd95380bcdd682e1
Nirbyoleek Das <nirbyoleek.das@tigeranalytics.com>
2024-08-02 13:37:26 +0530

    api endpoint added

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/users', userRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
